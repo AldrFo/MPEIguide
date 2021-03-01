c98c833d1683debcfdc883e4577f1394f67edd06
AlndrFo <mister.fo1996@gmail.com>
2021-03-01 22:26:03 +0300

    Описал классы Organization и Question

--- app/src/main/java/com/example/mpeiguide/info/faq/Question.java
+++ app/src/main/java/com/example/mpeiguide/info/faq/Question.java
@@ -2,5 +2,25 @@ package com.example.mpeiguide.info.faq;
 
 public class Question {
 
+    private String shortQuest;
     private String quest;
+    private String answer;
+
+    public Question(String shortQuest, String quest, String answer){
+        this.shortQuest = shortQuest;
+        this.quest = quest;
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getQuest() {
+        return quest;
+    }
+
+    public String getShortQuest() {
+        return shortQuest;
+    }
 }
--- app/src/main/java/com/example/mpeiguide/info/organizations/Organization.java
+++ app/src/main/java/com/example/mpeiguide/info/organizations/Organization.java
@@ -2,4 +2,54 @@ package com.example.mpeiguide.info.organizations;
 
 public class Organization {
 
+    public static final int NONE = 0;
+
+    private String orgName;
+    private String vkName;
+    private String vkLink;
+    private String president;
+    private String place;
+    private String description;
+
+    private int imageId;
+
+    public Organization(String orgName, String vkName, String vkLink, String president, String place,
+                        String description, int imageId){
+        this.orgName = orgName;
+        this.vkName = vkName;
+        this.vkLink = vkLink;
+        this.president = president;
+        this.place = place;
+        this.description = description;
+        this.imageId = imageId;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public String getVkName() {
+        return vkName;
+    }
+
+    public String getVkLink() {
+        return vkLink;
+    }
+
+    public String getPresident() {
+        return president;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
 }
