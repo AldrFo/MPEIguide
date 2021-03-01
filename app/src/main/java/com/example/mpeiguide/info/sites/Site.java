f7f548220e78796e04a59da68d59b0cce53ede70
AlndrFo <mister.fo1996@gmail.com>
2021-03-01 22:28:18 +0300

    Описал класс Site

--- app/src/main/java/com/example/mpeiguide/info/sites/Site.java
+++ app/src/main/java/com/example/mpeiguide/info/sites/Site.java
@@ -1,4 +1,26 @@
 package com.example.mpeiguide.info.sites;
 
 public class Site {
+
+    private String link;
+    private String name;
+    private String description;
+
+    public Site(String link, String name, String description){
+        this.link = link;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
