df08b779337380ffe9a5c01099c5face3a644725
AlndrFo <mister.fo1996@gmail.com>
2021-03-17 19:34:26 +0300

    Добавил часть контактов

--- app/src/main/java/com/example/mpeiguide/info/contacts/Contact.java
+++ app/src/main/java/com/example/mpeiguide/info/contacts/Contact.java
@@ -4,7 +4,140 @@ import java.util.ArrayList;
 
 public class Contact {
 
-    public static Contact[] contacts ={
+    public static Contact[] contacts = {
+            new Contact(
+                    "Бобряков Александр Владимирович",
+                    "Заведующий кафедрой управления и интеллектуальных технологий",
+                    "+7 495 362-74-07",
+                    "М-313","",
+                    "BobriakovAV@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Варшавский Павел Романович",
+                    "Заведующий кафедрой прикладной математики и искусственного интеллекта",
+                    "+7 495 362-79-62, 61-14",
+                    "М-703","",
+                    "VarshavskyPR@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Вишняков Сергей Викторович",
+                    "Директор, заведующий кафедрой вычислительных машин, систем и сетей",
+                    "+7 495 362-75-39, 76-64",
+                    "Г-313","",
+                    "VishniakovSV@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Желбаков Игорь Николаевич",
+                    "Заведующий кафедрой диагностических информационных технологий",
+                    "+7 495 362-77-47",
+                    "В-310","",
+                    "ZhelbakovIgN@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Зубков Павел Валерьевич",
+                    "Заведующий кафедрой математического и компьютерного моделирования",
+                    "+7 495 362-77-72",
+                    "М-705","",
+                    "ZubkovPV@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Коротких Татьяна Николаевна",
+                    "Начальник 2 курса (бакалавриат)",
+                    "+7 495 362-76-64",
+                    "Г-317","",
+                    "KorotkikhTN@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Костина Мария Борисовна",
+                    "Начальник 3 курса (бакалавриат) и 1 курса (магистратура)",
+                    "+7 495 362-76-64",
+                    "Г-315","",
+                    "KostinaMB@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Русинова Наталия Николаевна",
+                    "Начальник 2 курса (магистратура)",
+                    "+7 495 362-76-64",
+                    "Г-315","",
+                    "RusinovaNN@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Топорков Виктор Васильевич",
+                    "Заведующий кафедрой вычислительных технологий",
+                    "+7 495 362-71-45",
+                    "","",
+                    "ToporkovVV@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Шагов Никита Сергеевич",
+                    "Начальник 4 курса (бакалавриат)",
+                    "+7 495 362-76-64",
+                    "Г-313","",
+                    "ShagovNS@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Щёголев Павел",
+                    "Начальник 1 курса (бакалавриат)",
+                    "+7 495 362-76-64",
+                    "Г-315","",
+                    "ShchegolevsP@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Абросимов Леонид Иванович",
+                    "Профессор",
+                    "",
+                    "","",
+                    "AbrosimovLI@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Борисова Светлана Вячеславовна",
+                    "Старший преподаватель",
+                    "",
+                    "","",
+                    "BorisovaSV@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Геворкян Владимир Мушегович",
+                    "Профессор",
+                    "",
+                    "","",
+                    "GevorkianVM@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Казанцев Юрий Алексеевич",
+                    "Профессор",
+                    "",
+                    "","",
+                    "KazantsevYA@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Кобяк Александр Трофимович",
+                    "Доцент",
+                    "",
+                    "","",
+                    "KobiakAT@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Ковалева Татьяна Юрьевна",
+                    "Доцент",
+                    "",
+                    "","",
+                    "KovalevaTY@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Краюшкин Владимир Валентинович",
+                    "Доцент",
+                    "",
+                    "","",
+                    "KrayushkinVV@mpei.ru",
+                    Contact.NONE),
+            new Contact(
+                    "Ладыгин Игорь Иванович",
+                    "Профессор",
+                    "",
+                    "","",
+                    "LadyginII@mpei.ru",
+                    Contact.NONE),
     };
 
     public static final int NONE = 0;
